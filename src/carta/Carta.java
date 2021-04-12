c8735ae6c0c140126afda6db65663e2f0dca5c53
Karlos Daniel Pires da Silva <karlosdanielsilva7654@gmail.com>
2021-04-12 13:19:55 -0300

    Atualiza Carta.java Define o package de src/carta/Carta.java como carta; Cria a classe Carta; Cria os atributos de visibilidade protected id, set, nome, descricao e imageSrc; Cria o construtor para os atributos id, set, nome e descricao; Cria os getters de todos da classe Carta (id, set, nome, descricao e imageSrc); Cria os setters dos atributos nome, descricao e imageSrc.

---
+++ src/carta/Carta.java
@@ -0,0 +1,52 @@
+package carta;
+
+public class Carta {
+
+    protected int id;           // Id da carta em relacao ao seu set
+    protected String set = "";  // Fonte: https://forum.yugiohcardmaker.net/topic/7296-set-id/
+    protected String nome;
+    protected String descricao = "";
+    protected String imageSrc = null;
+
+    public Carta(int id, String set, String nome, String descricao) {
+        this.id = id;
+        this.set = set;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public String getSet() {
+        return set;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getImageSrc() {
+        return imageSrc;
+    }
+    
+    public void setImageSrc(String imageSrc) {
+        this.imageSrc = imageSrc;
+    }
+    
+    
+}
+
