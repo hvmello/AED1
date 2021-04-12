06a204b9e4cf75361a2ce329428a2143f09c3e9b
Karlos Daniel Pires da Silva <karlosdanielsilva7654@gmail.com>
2021-04-12 20:07:31 -0300

    Atualiza CartaMonstro.java Faz a classe CartaMagica herdar de Carta; Define o enum estatico TipoMonstro com os valores BESTA, HUMANO, ANJO e DEMONIO na classe CartaMonstro; Define o enum estatico TipoAtributo com os valores DARK, LIGHT, WIND, EARTH, WATER, e FIRE na classe CartaMonstro; Define o enum ModoCarta com os valores ATAQUE_PARA_CIMA, ATAQUE_PARA_BAIXO, DEFESA_PARA_CIMA e DEFESA_PARA_BAIXO na classe CartaMonstro; Declara o atributo tipoMonstro em CartaMonstro; Declara o atributo tipoAtributo em CartaMonstro; Declara o atributo modoCarta em CartaMonstro; Declara o atributo quantidadeEstrelas em CartaMonstro e define o seu valor default como 0; Declara o atributo ATK em CartaMonstro; Declara o atributo DEF em CartaMonstro; Cria o construtor de CartaMonstro que recebe como parametros valores para os atributos herdados de Carta e os inicializa com o super(); Cria os getters dos atributos tipoMonstro, tipoAtributo, modoCarta, quantidadeEstrelas, ATK e DEF; Cria os setters dos atributos tipoAtributo, modoCarta e quantidadeEstrelas.

--- src/carta/CartaMonstro.java
+++ src/carta/CartaMonstro.java
@@ -1,5 +1,64 @@
 package carta;
 
-public class CartaMonstro {
+public class CartaMonstro extends Carta {
+
+    public static enum TipoMonstro {
+        BESTA, HUMANO, ANJO, DEMONIO;
+    }
     
+    public static enum TipoAtributo {
+        DARK, LIGHT, WIND, EARTH, WATER, FIRE;
+    }
+    
+    public static enum ModoCarta {
+        ATAQUE_PARA_CIMA, ATAQUE_PARA_BAIXO, DEFESA_PARA_CIMA, DEFESA_PARA_BAIXO;
+    }
+    
+    private TipoMonstro tipoMonstro;
+    private TipoAtributo tipoAtributo;
+    private ModoCarta modoCarta;
+    private int quantidadeEstrelas = 0;
+    private int ATK;
+    private int DEF;
+
+    public CartaMonstro(int id, String set, String nome, String descricao) {
+        super(id, set, nome, descricao);
+    }
+
+    public TipoMonstro getTipoMonstro() {
+        return tipoMonstro;
+    }
+
+    public TipoAtributo getTipoAtributo() {
+        return tipoAtributo;
+    }
+
+    public void setTipoAtributo(TipoAtributo tipoAtributo) {
+        this.tipoAtributo = tipoAtributo;
+    }
+
+    public ModoCarta getModoCarta() {
+        return modoCarta;
+    }
+
+    public void setModoCarta(ModoCarta modoCarta) {
+        this.modoCarta = modoCarta;
+    }
+
+    public int getQuantidadeEstrelas() {
+        return quantidadeEstrelas;
+    }
+
+    public void setQuantidadeEstrelas(int quantidadeEstrelas) {
+        this.quantidadeEstrelas = quantidadeEstrelas;
+    }
+
+    public int getATK() {
+        return ATK;
+    }
+
+    public int getDEF() {
+        return DEF;
+    }
+
 }
