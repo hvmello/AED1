4607daeb2d1f8d0f5caf10038e292f9d2950f42f
Karlos Daniel Pires da Silva <karlosdanielsilva7654@gmail.com>
2021-04-12 18:37:40 -0300

    Atualiza CartaMagica.java Faz a classe CartaMagica herdar de Carta; Define o enum estatico TipoEfeitoMagico com os valores CAMPO e TRAP na classe CartaMagica; Declara o atributo tipoEfeitoMagico em CartaMagica; Cria o construtor de CartaMagica que recebe como parametros valores para os atributos herdados de Carta e os inicializa com o super(); Cria o getter do atributo tipoEfeitoMagico.

--- src/carta/CartaMagica.java
+++ src/carta/CartaMagica.java
@@ -1,5 +1,32 @@
 package carta;
 
-public class CartaMagica {
+public class CartaMagica extends Carta {
+    
+    public static enum TipoEfeitoMagico {
+        CAMPO(0), TRAP(1);
+        
+        private final int value;
+        
+        private TipoEfeitoMagico(int value) {
+            this.value = value;
+        }
+        
+        public int getValue() {
+            return value;
+        }
+    }
+    
+    private TipoEfeitoMagico tipoEfeitoMagico;
+    
+    
+    public CartaMagica(int id, String set, String nome, String descricao) {
+        super(id, set, nome, descricao);
+    }
+
+    public TipoEfeitoMagico getTipoEfeitoMagico() {
+        return tipoEfeitoMagico;
+    }
+    
     
 }
+
