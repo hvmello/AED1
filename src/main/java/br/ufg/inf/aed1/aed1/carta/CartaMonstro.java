bfff26f3460a8a0dcb6fb09fc2ed1b83bb0be614
Karlos Daniel Pires da Silva <karlosdanielsilva7654@gmail.com>
2021-05-23 11:19:54 -0300

    Atualiza CartaMonstro.java
    - Altera o construtor da classe CartaMonstro, fazendo a inicializaçao
      dos atributos necessários e tratando uma possível exceção na obtenção
      da imagem da carta;
    
    - Adiciona os métodos virarParaCima(), viraParaBaixo(), isModoAtaque(),
      isModoDefesa() na classe CartaMonstro.

--- src/main/java/br/ufg/inf/aed1/aed1/carta/CartaMonstro.java
+++ src/main/java/br/ufg/inf/aed1/aed1/carta/CartaMonstro.java
@@ -1,38 +1,39 @@
 package br.ufg.inf.aed1.aed1.carta;
 
+import br.ufg.inf.aed1.aed1.projeto.GetCardImage;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class CartaMonstro extends Carta {
 
-  
     public static enum TipoMonstro {
         BESTA, HUMANO, ANJO, DEMONIO;
     }
-    
+
     public static enum TipoAtributo {
         DARK, LIGHT, WIND, EARTH, WATER, FIRE;
     }
-    
+
     public static enum ModoCarta {
         ATAQUE_PARA_CIMA, ATAQUE_PARA_BAIXO, DEFESA_PARA_CIMA, DEFESA_PARA_BAIXO;
-        
+
         public boolean isAtaqueCima() {
             return this.equals(ModoCarta.ATAQUE_PARA_CIMA);
         }
-        
+
         public boolean isAtaqueBaixo() {
             return this.equals(ModoCarta.ATAQUE_PARA_BAIXO);
         }
-        
+
         public boolean isDefesaCima() {
             return this.equals(ModoCarta.DEFESA_PARA_CIMA);
         }
-        
+
         public boolean isDefesaBaixo() {
             return this.equals(ModoCarta.DEFESA_PARA_BAIXO);
         }
     }
-    
-    
-    
+
     private TipoMonstro tipoMonstro;
     private TipoAtributo tipoAtributo;
     private ModoCarta modoCarta;
@@ -40,8 +41,48 @@ public class CartaMonstro extends Carta {
     private int ATK;
     private int DEF;
 
-    public CartaMonstro(int id, String set, String nome, String descricao) {
+    public CartaMonstro(int id, String set, String nome, String descricao, int ATK, int DEF,
+            TipoMonstro tipoMonstro, TipoAtributo tipoAtributo, String imageSrc) {
+
         super(id, set, nome, descricao);
+
+        String imgSrcFinal;
+
+        this.ATK = ATK;
+        this.DEF = DEF;
+        this.tipoMonstro = tipoMonstro;
+        this.tipoAtributo = tipoAtributo;
+
+        try {
+            imgSrcFinal = GetCardImage.httpGetImage(this, imageSrc);
+            this.setImageSrc(imgSrcFinal);
+        } catch (Exception exception) {
+            Logger.getLogger(Carta.class.getName()).log(Level.SEVERE, null, exception);
+        }
+    }
+
+    public void virarParaCima() {
+        if (isModoAtaque()) {
+            modoCarta = ModoCarta.ATAQUE_PARA_CIMA;
+        } else {
+            modoCarta = ModoCarta.DEFESA_PARA_CIMA;
+        }
+    }
+
+    public void virarParaBaixo() {
+        if (isModoAtaque()) {
+            modoCarta = ModoCarta.ATAQUE_PARA_BAIXO;
+        } else {
+            modoCarta = ModoCarta.ATAQUE_PARA_BAIXO;
+        }
+    }
+
+    public boolean isModoAtaque() {
+        return modoCarta == ModoCarta.ATAQUE_PARA_BAIXO || modoCarta == ModoCarta.ATAQUE_PARA_CIMA;
+    }
+
+    public boolean isModoDefesa() {
+        return modoCarta == ModoCarta.DEFESA_PARA_BAIXO || modoCarta == ModoCarta.DEFESA_PARA_CIMA;
     }
 
     public TipoMonstro getTipoMonstro() {
